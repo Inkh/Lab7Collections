b95bfa300f5702dec971fcb2a6696f0ea8e34e08
inkh <jhwe92@gmail.com>
2018-10-17 21:43:03 -0700

    Added returnsuit method

--- Collections/Collections/Classes/Deck.cs
+++ Collections/Collections/Classes/Deck.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Collections.Classes
 {
-    public class Deck<T>
+    public class Deck<T> : IEnumerable
     {
         T[] cards = new T[5];
         int count = 0;
@@ -20,6 +21,45 @@ namespace Collections.Classes
                 Array.Resize(ref cards, cards.Length * 2);
             }
             cards[count++] = card;
+        }
+
+        /// <summary>
+        /// Returns a list of cards of the same suit from deck.
+        /// </summary>
+        /// <param name="suit">Name of suit (Spades, Hearts, Diamonds, Clovers)</param>
+        /// <returns>Array of cards of same suit</returns>
+        public Card[] ReturnSuit(Suit suit)
+        {
+            Card[] cardsOfSameSuit = new Card[cards.Length];
+            int sizeOfList = 0;
+            for (int i = 0; i < count; i++)
+            {
+                Card cardOfSuit = (Card)Convert.ChangeType(cards[i], typeof(Card));
+
+                if(cardOfSuit.Suit == suit)
+                {
+                    cardsOfSameSuit[i] = cardOfSuit;
+                    sizeOfList++;
+                }
+            }
+            Array.Resize(ref cardsOfSameSuit, sizeOfList - 1);
+
+            return cardsOfSameSuit;
+        }
+
+
+        //magic don't touch
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return cards[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Collections.Classes;
 
 namespace Collections
@@ -13,11 +14,48 @@ namespace Collections
 
         static void Test()
         {
-            Card ace = new Card(Suit.Spades, Value.Ace);
+            Card aceOfSpades = new Card(Suit.Spades, Value.Ace);
+            Card twoOfDiamonds = new Card(Suit.Diamonds, Value.Two);
+            Card kingOfHearts = new Card(Suit.Hearts, Value.King);
+            Card queenOfDiamonds = new Card(Suit.Diamonds, Value.Queen);
+            Card sixOfClovers = new Card(Suit.Clovers, Value.Six);
+            Card fourOfDiamonds = new Card(Suit.Diamonds, Value.Four);
+            Card fiveOfSpades = new Card(Suit.Spades, Value.Five);
+            Card tenOfHearts = new Card(Suit.Hearts, Value.Ten);
+            Card aceOfDiamonds = new Card(Suit.Diamonds, Value.Ace);
+            Card aceOfHearts = new Card(Suit.Hearts, Value.Ace);
+
+            List<Card> myCards = new List<Card>
+            {
+                aceOfSpades,
+                twoOfDiamonds,
+                kingOfHearts,
+                queenOfDiamonds,
+                sixOfClovers,
+                fourOfDiamonds,
+                fiveOfSpades,
+                tenOfHearts,
+                aceOfDiamonds,
+                aceOfHearts
+            };
+
+
             Deck<Card> myDeck = new Deck<Card>();
-            myDeck.Add(ace);
-            
-            Console.WriteLine($"{(int)ace.Value} of {ace.Suit}");
+
+            //Add cards to list
+            foreach (Card card in myCards)
+            {
+                myDeck.Add(card);
+            }
+
+            Card[] cardsOfSuit = myDeck.ReturnSuit(Suit.Hearts);
+
+            foreach (Card card in cardsOfSuit)
+            {
+                //Console.WriteLine($"{card.Value} of {card.Suit}");
+                Console.WriteLine(card.Value);
+            }
+            //Console.WriteLine($"{aceOfSpades.Value} of {aceOfSpades.Suit}");
             
             
         }
