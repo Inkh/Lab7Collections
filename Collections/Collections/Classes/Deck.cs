29af073ad466f8fa5843866d34bc3c42c34791d3
inkh <jhwe92@gmail.com>
2018-10-17 20:55:47 -0700

    Created instance of Deck and card.

---
+++ Collections/Collections/Classes/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections.Classes
+{
+    public class Deck<T>
+    {
+        T[] cards = new T[5];
+        int count = 0;
+
+        /// <summary>
+        /// Adds card to Deck
+        /// </summary>
+        /// <param name="card">Card object</param>
+        public void Add(T card)
+        {
+            if (count == cards.Length)
+            {
+                Array.Resize(ref cards, cards.Length * 2);
+            }
+            cards[count++] = card;
+        }
+    }
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Collections.Classes;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+
+        static void Test()
+        {
+            Card ace = new Card(Suit.Spades, Value.Ace);
+            Deck<Card> myDeck = new Deck<Card>();
+            myDeck.Add(ace);
+            
+            Console.WriteLine($"{(int)ace.Value} of {ace.Suit}");
+            
+            
+        }
+    }
+}
